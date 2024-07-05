ed3fe95fd92731332cfc29132340f21e85e9b6d0
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-07-06 02:48:35 +0300

    form page init

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
           />
 
           <PatientForm />
+
           <div className="text-14-regular mt-20 flex justify-between">
               <p className="justify-items-end text-dark-600 xl:text-left"> Hello There, HealthWise</p>
               <Link href="/?admin=true" className="text-green-500">
